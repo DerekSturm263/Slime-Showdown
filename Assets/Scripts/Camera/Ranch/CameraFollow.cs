9e090f93b1c67998ce923a2d4a733c297b2f85bb
Derek Sturm <dereksturm263@gmail.com>
2020-04-03 11:57:41 -0700

    Added new sprites for UI elements. Will probably change a few cause I don't like how they look. Gonna work on the shop next and obstacles in the overworld that the player can't pass through. Might also add slopes for the player to go up.

--- Assets/Scripts/Camera/Ranch/CameraFollow.cs
+++ Assets/Scripts/Camera/Ranch/CameraFollow.cs
@@ -6,9 +6,10 @@ public class CameraFollow : MonoBehaviour
 {
     public GameObject cameraTarget;
     public float cameraSpeed;
+    public float cameraDistance;
 
     private void FixedUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, cameraTarget.transform.position - new Vector3(0, 0, 5), cameraSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, cameraTarget.transform.position - new Vector3(0, 0, cameraDistance), cameraSpeed * Time.deltaTime);
     }
 }
