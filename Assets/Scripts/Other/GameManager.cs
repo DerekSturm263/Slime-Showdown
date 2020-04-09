6e82c0337feba8fcda8415067447c110c9b62331
Derek Sturm <dereksturm263@gmail.com>
2020-04-09 00:23:54 -0700

    Added some 2D lighting stuff.

--- Assets/Scripts/Other/GameManager.cs
+++ Assets/Scripts/Other/GameManager.cs
@@ -11,15 +11,26 @@ public class GameManager : MonoBehaviour
 
     #endregion
 
+    [Header("Slime Stuff")]
     public string playerSlimeColor;
     public string playerSlimeName;
+
     public int playSourAff;
     public int playSpicyAff;
     public int playSeafoodAff;
     public int playCandyAff;
     public int payVeggieAff;
 
+    [Header("Ranch Stuff")]
     public uint goldCount;
 
+    public float sunRotSpeed;
+    public float sunRot;
+
     public GameObject[] inventory = new GameObject[27];
+
+    private void FixedUpdate()
+    {
+        sunRot += sunRotSpeed * Time.deltaTime;
+    }
 }
---
+++ Assets/Scripts/Other/MoveSun.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSun : MonoBehaviour
+{
+    private GameObject gameManager;
+    private GameManager gameManagerScript;
+
+    private void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameController");
+        gameManagerScript = gameManager.GetComponent<GameManager>();
+    }
+
+    private void Update()
+    {
+        transform.localEulerAngles = new Vector3(0f, 0f, gameManagerScript.sunRot);
+    }
+}
