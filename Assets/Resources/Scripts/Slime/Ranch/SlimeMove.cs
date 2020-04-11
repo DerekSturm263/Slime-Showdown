8214a75bf4aa4bc93ca8aedbf47e9ad410d43cac
Derek Sturm <dereksturm263@gmail.com>
2020-04-11 16:19:27 -0700

    Added in animator controller for the pink slime. Will add the rest of the animator controllers in the next push. Added colliders back to the ranch. Will also fix the shop in the next push.

--- Assets/Resources/Scripts/Slime/Ranch/SlimeMove.cs
+++ Assets/Resources/Scripts/Slime/Ranch/SlimeMove.cs
@@ -6,15 +6,17 @@ public class SlimeMove : MonoBehaviour
 {
     private GameObject gameManager;
 
-    public enum State
+    private enum State
     {
         Idle, Moving
     }
+    private State animState = State.Idle;
 
-    public enum Direction
+    private enum Direction
     {
         Up, Down, Left, Right
     }
+    private Direction animDirection = Direction.Down;
 
     [SerializeField]
     private float walkSpeed = 1f;
@@ -32,11 +34,7 @@ public class SlimeMove : MonoBehaviour
         gameManager = GameObject.FindGameObjectWithTag("GameController");
 
         animController = GetComponent<Animator>();
-
         string animName = "Animations/Slime/Ranch/" + gameManager.GetComponent<GameManager>().playerSlimeColor + " Slime/slime_" + gameManager.GetComponent<GameManager>().playerSlimeColor.ToLower() + "_ranch";
-
-        Debug.Log(animName);
-
         animController.runtimeAnimatorController = Resources.Load(animName) as RuntimeAnimatorController;
 
         rb2D = GetComponent<Rigidbody2D>();
@@ -44,14 +42,39 @@ public class SlimeMove : MonoBehaviour
 
     private void FixedUpdate()
     {
-         if (Input.GetButton("Run"))
+        #region Position Changing
+
+        if (Input.GetButton("Run"))
              moveSpeed = runSpeed;
-         else
+        else
              moveSpeed = walkSpeed;
 
-         moveAmount.x = Mathf.Lerp(0, Input.GetAxis("Horizontal") * moveSpeed, Mathf.Abs(Input.GetAxis("Horizontal")));
-         moveAmount.y = Mathf.Lerp(0, Input.GetAxis("Vertical") * moveSpeed, Mathf.Abs(Input.GetAxis("Vertical")));
+        moveAmount.x = Mathf.Lerp(0f, Input.GetAxis("Horizontal") * moveSpeed, Mathf.Abs(Input.GetAxis("Horizontal")));
+        moveAmount.y = Mathf.Lerp(0f, Input.GetAxis("Vertical") * moveSpeed, Mathf.Abs(Input.GetAxis("Vertical")));
+        
+        rb2D.velocity = moveAmount;
 
-         rb2D.velocity = moveAmount;
+        #endregion
+
+        #region Animation States
+
+        if (moveAmount.x == 0f && moveAmount.y == 0f)
+            animState = State.Idle;
+        else
+            animState = State.Moving;
+
+        if (moveAmount.y > 0f)
+            animDirection = Direction.Up;
+        else if (moveAmount.y < 0f)
+            animDirection = Direction.Down;
+        else if (moveAmount.x < 0f)
+            animDirection = Direction.Left;
+        else if (moveAmount.x > 0f)
+            animDirection = Direction.Right;
+
+        animController.SetInteger("State", (int) animState);
+        animController.SetInteger("Direction", (int) animDirection);
+
+        #endregion
     }
 }
