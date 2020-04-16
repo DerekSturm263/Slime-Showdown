fe957658fbd96cb073c20b631c9fd537f62555f2
Joseph Huskey <josephhuskey@Josephs-MacBook-Air.local>
2020-04-15 17:49:24 -0700

    Fixed initiator testing gui in battle scene

--- Assets/Resources/Scripts/Battle/MoveToBattle.cs
+++ Assets/Resources/Scripts/Battle/MoveToBattle.cs
@@ -6,10 +6,8 @@ using UnityEngine.SceneManagement;
 public class MoveToBattle : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "RanchBattleSlime")
-        {
+    { 
             SceneManager.LoadScene("Battle");
-        }
+       
     }
 }
