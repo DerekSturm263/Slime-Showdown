cd59c2a83caee5f236a8b7c188ddcef3d5d8f3aa
Derek Sturm <dereksturm263@gmail.com>
2020-04-24 22:49:33 -0700

    Did stuff

--- Assets/Resources/Scripts/Other/AnimateStatsIdle.cs
+++ Assets/Resources/Scripts/Other/AnimateStatsIdle.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AnimateStatsIdle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject gameManager;
+
+    private Image image;
+    private SpriteRenderer sprtRndr;
+    private Animator animController;
+
+    private void Start()
     {
-        
+        gameManager = GameObject.FindGameObjectWithTag("GameController");
+
+        string animName = "Animations/Slime/Choose Slime/" + gameManager.GetComponent<GameManager>().playerSlimeColor + " Slime/Eating/slime_" + gameManager.GetComponent<GameManager>().playerSlimeColor.ToLower() + "_pickSlime";
+
+        image = GetComponent<Image>();
+        sprtRndr = GetComponent<SpriteRenderer>();
+        animController = GetComponent<Animator>();
+
+        animController.runtimeAnimatorController = Resources.Load(animName) as RuntimeAnimatorController;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        image.sprite = sprtRndr.sprite;
     }
 }
